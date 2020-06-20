6f9f85a9321c77f570b2bbf463ff0aa72ddc7bca
Tina Baumgärtel <tina.baumgaertel@hs-furtwangen.de>
2020-06-20 21:07:26 +0200

    Aufgabe08

--- Aufgabe08/server.js
+++ Aufgabe08/server.js
@@ -1,25 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.A08Server = void 0;
-const Http = require("http");
+const Http = require("http"); // ein url link wird aufgerufen
 var A08Server;
 (function (A08Server) {
     console.log("Starting server");
     let port = Number(process.env.PORT);
-    if (!port)
+    if (!port) //wenn es kein Port gibt wird die zahl 8100 ausgegeben. 
         port = 8100;
-    let server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
+    let server = Http.createServer(); // neuer server wird erstellt 
+    server.addListener("request", handleRequest); //anfragen bearbeiten 
+    server.addListener("listening", handleListen); //anfragen abfangen
     server.listen(port);
+    //Ausführen bei Start
     function handleListen() {
-        console.log("Listening");
+        console.log("Listening"); //Methoden aufruf ohne Rückgabewert
     }
+    //Ausführen bei Anfragen
     function handleRequest(_request, _response) {
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
+        console.log("I hear voices!"); //Methodenaufruf
+        //Parameter erstellen und den Kopf des Dokuments (http-Header)
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //gibt die Zeichncordierung des Dokuments an und gibt den Conten-Typ an
+        _response.setHeader("Access-Control-Allow-Origin", "*"); // gibt an, ob die Antwort mit dem Anfordern von Code vom angegebenen Ursprung geteilt werden kann.
+        _response.write(_request.url); // als Anforderungs-Antwort-Protokoll zwischen einem Client und einem Server.
         _response.end();
     }
 })(A08Server = exports.A08Server || (exports.A08Server = {}));
--- Aufgabe08/server.ts
+++ Aufgabe08/server.ts
@@ -1,28 +1,31 @@
 
-import * as Http from "http";
+import * as Http from "http"; // ein url link wird aufgerufen
 
-export namespace A08Server {
+export namespace A08Server { // wird auf Funktionen die importiert werden geprüft
     console.log("Starting server");
     let port: number = Number(process.env.PORT);
-    if (!port)
+    if (!port) //wenn es kein Port gibt wird die zahl 8100 ausgegeben. 
         port = 8100;
 
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
+    let server: Http.Server = Http.createServer(); // neuer server wird erstellt 
+    server.addListener("request", handleRequest); //anfragen bearbeiten 
+    server.addListener("listening", handleListen); //anfragen abfangen
+    server.listen(port); 
+
+    //Ausführen bei Start
 
     function handleListen(): void {
-        console.log("Listening");
+        console.log("Listening"); //Methoden aufruf ohne Rückgabewert
     }
-
+//Ausführen bei Anfragen
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
+        console.log("I hear voices!"); //Methodenaufruf
 
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
+        //Parameter erstellen und den Kopf des Dokuments (http-Header)
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //gibt die Zeichncordierung des Dokuments an und gibt den Conten-Typ an
+        _response.setHeader("Access-Control-Allow-Origin", "*"); // gibt an, ob die Antwort mit dem Anfordern von Code vom angegebenen Ursprung geteilt werden kann.
 
-        _response.write(_request.url);
+        _response.write(_request.url); // als Anforderungs-Antwort-Protokoll zwischen einem Client und einem Server.
 
         _response.end();
     }
